9c6b96beefe7267fc2b9b8cb11dd4140a7214a70
João Otávio Biondo <joao.otb@gmail.com>
2021-06-08 09:14:20 -0300

    Get the correct type of originator_id when building versions for update

--- lib/paper_trail/serializer.ex
+++ lib/paper_trail/serializer.ex
@@ -78,6 +78,7 @@ defmodule PaperTrail.Serializer do
     originator = RepoClient.originator()
     originator_ref = options[originator[:name]] || options[:originator]
     originator_id = if(originator_ref, do: originator_ref.id, else: nil)
+    originator_id_type = RepoClient.originator_type()
     origin = options[:origin]
     meta = options[:meta]
     repo = RepoClient.repo(options)
@@ -89,7 +90,7 @@ defmodule PaperTrail.Serializer do
           item_type: type(^item_type, :string),
           item_id: field(q, ^primary_key),
           item_changes: type(^changes_map, :map),
-          originator_id: type(^originator_id, :string),
+          originator_id: type(^originator_id, ^originator_id_type),
           origin: type(^origin, :string),
           meta: type(^meta, :map),
           inserted_at: type(fragment("CURRENT_TIMESTAMP"), :naive_datetime)
